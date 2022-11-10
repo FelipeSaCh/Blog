5f143902606fbc6a487ee9d2db4d657f2115768d
FelipeSaCh <andres.sanchez346@pascualbravo.edu.co>
2022-11-09 21:03:54 -0500

    V.F

--- Articulos/models.py
+++ Articulos/models.py
@@ -1,8 +1,10 @@
 from django.db import models
-
 # Create your models here.
 class Entrada(models.Model):
     nombre= models.CharField(max_length=50)
-    contenido = models.TextField(max_length=400)
+    contenido = models.TextField(max_length=1000)
     imagen=models.URLField()
     autor=models.CharField(max_length=50)
+
+    def __str__(self) :
+        return  self.nombre
--- Articulos/views.py
+++ Articulos/views.py
@@ -1,5 +1,7 @@
 from django.shortcuts import render
+from Articulos.models import Entrada
 
 # Create your views here.
 def home(request):
-    return render(request,"bienvenida.html")
+    articulos=Entrada.objects.all()
+    return render(request,"bienvenida.html",{"articulos":articulos})
